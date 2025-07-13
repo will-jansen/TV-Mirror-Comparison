8ef3bc877ab8348cc1aeb46f52ac1c0c87170ce9
will-jansen <73018431+will-jansen@users.noreply.github.com>
2025-07-13 17:16:26 +1000

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+// script.js
+// This file is currently empty as there's no dynamic JavaScript functionality needed for this comparison page.
+// You can add any interactive elements or dynamic content here in the future.
